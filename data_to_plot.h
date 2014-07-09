7d769f57fe615ad341d39bf32a0eb7fc9455d886
Alex  Jennings <alexjennings@10-196-45-244.wireless.oregonstate.edu>
2014-07-09 11:31:59 -0700

    Progress on Header File Member Functions
    Filling in the blanks for the Statistics_Library class member functions.

--- data_to_plot.h
+++ data_to_plot.h
@@ -13,8 +13,11 @@
 /// DOCUMENTATION:
 /// AJ
 
+using namespace std;
+#include <iostream>
+
 class statistics_library{
-    void carriage_return();
+    void carriage_return(vector<double>);
     void take_value(double);
     
     vector<double> single_stat_run;
@@ -26,8 +29,8 @@ class statistics_library{
     vector<double> running_means;
     void clear_all();
     
-    void calc_means();
-    void calc_medians();
+    void calc_means(int,int);
+    void calc_medians(int,int);
     void calc_stdevs();
     void calc_running_means();
     
@@ -35,28 +38,57 @@ class statistics_library{
     void push_to_file();
 };
 
-void clear_all(){
+void statistics_library::take_value(double action_value){
+    vector<double> value_taken;
+    value_taken.push_back (action_value);
     /// AJ
 }
 
-void carriage_return(){
+void statistics_library::carriage_return(vector<double> take_value){
+     master_table.push_back(take_value);
+    take_value.clear();
+    
     /// AJ
 }
 
-void take_value(double val){
+
+}
+
+void statistics_library::calc_means(int nmbr_runs, int nmbr_iterations){
+    double total=0;
+    vector<double> means;
+    double mean;
+    for (int n=0; n<nmbr_runs; n++) {
+        vector <double> rows;
+        rows=(master_table.at(n));
+        for (int k=0; k<nmbr_iterations; k++){
+            total+=rows.at(k);
+        }
+        mean=total/nmbr_iterations;
+         means.push_back(mean);
+    }
     /// AJ
 }
 
-void calc_means(){
-    /// AJ
-}
-
-void calc_medians(){
+void statistics_library::calc_medians(int nmbr_runs, int nmbr_iterations){
+    vector<double> medians;
+    int x;
+    double median;
+    for (int n=0; n<nmbr_runs; n++) {
+        vector <double> rows;
+        rows=(master_table.at(n));
+        sort(rows.begin(),rows.end());
+        x=nmbr_iterations/2;
+        median=(rows.at(x)+rows.at(x+1))/2;
+        medians.push_back(median);
+    }
+    
     /// AJ
 }
 
 void calc_stdevs(){
     /// AJ
+    
 }
 
 void calc_running_means(){
@@ -66,5 +98,21 @@ void calc_running_means(){
 void push_to_file(){
     /// AJ
 }
+
+// Define class, stats_lib
+//  class stats_lib{
+//     Define the vector-building function, recieve_score
+//      double recieve_score (double rwrd_val)
+//          {vector<double> pulled_rwrd
+//          pulled_rwrd.push_back(rwrd_val)
+//      } end function
+//     Define function, table-buidling function carriage_return
+//      double carriage_return (pulled_rwrd)
+//          {vector<vector<double> > stats_table [THIS IS A TABLE (VECTOR OF VECTORS)]
+//
+//      } end function
+//Define function, stats_ops
+// double stats_ops (
+
 
 #endif
